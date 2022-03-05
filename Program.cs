de2907bf9737e44a93952712ea67db7227618f06
Ricp77 <58709558+Ricp77@users.noreply.github.com>
2022-03-05 20:38:15 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MyCourse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            string firstofthearguments = args.FirstOrDefault();
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
